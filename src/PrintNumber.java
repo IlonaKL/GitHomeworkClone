86b2a929aa0710fffcdc72bdd1b8892db92e3998
IlonaKL <ilonalitv@gmail.com>
2022-05-18 21:55:25 -0400

    updated code

--- src/PrintNumber.java
+++ src/PrintNumber.java
@@ -1,6 +1,6 @@
 public class PrintNumber {
     public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 50; i++) {
             System.out.print(i+" ");
         }
     }
